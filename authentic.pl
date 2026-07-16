1c6ec1bbb6c21fde27b3fe8c88e0ab8d5e20090c
Ilia Ross <ilia@ross.gdn>
2026-07-16 20:11:33 +0200

    Fix bottom buttons align

--- authentic.pl
+++ authentic.pl
@@ -193,7 +193,9 @@ sub theme_footer
             $url = $url . "/"             if ($url =~ /[^\/]$/ && $url !~ /.cgi/ && $url !~ /javascript:history/ && $url !~ /[&?]/);
             print
 "<a style='margin-bottom: 10px;' class='btn btn-primary btn-lg page_footer_submit' href=\"$url\"><i class='fa fa-fw fa-arrow-left'>&nbsp;</i> <span>",
-              &text('main_return', $_[$i + 1]), "</span></a>&nbsp;&nbsp;\n";
+              &text('main_return', $_[$i + 1]), "</span></a>".
+                &ui_tag('span', undef,
+                    { style => 'display: inline-block; width: 4px' })."\n";
         }
     }
 
